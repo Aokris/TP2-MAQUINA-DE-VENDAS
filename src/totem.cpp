894ebf9dc5ee0e2b7874f9375da5f50626f6e54e
RuhannCP <ruhannc@ufmg.br>
2019-11-15 16:56:48 -0200

    Feito a opção 2

--- src/totem.cpp
+++ src/totem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 #include "totem.h"
 
 using namespace std;
@@ -16,8 +17,35 @@ void Totem::InicializarEstruturasEvento(){
 }
 
 //listar todos os usuarios cadastrados
-void Totem::ListarUsuarios(){
+void Totem::ListarUsuarios (vector<Crianca> cria, vector<Adulto> adult, vector<Idoso> idos){
+    vector<Crianca>::iterator itCria;
+    vector<Adulto>::iterator itAdul;
+    vector<Idoso>::iterator itIdos;
 
+    for (itCria = cria.begin(); itCria != cria.end();itCria++){//Impressão de todas as crianças
+        std::cout << endl << itCria->get_id() << " ";
+        std::cout << itCria->get_nome() << " ";
+        std::cout << itCria->get_idade() << " ";
+        std::cout << itCria->get_id_responsavel() << " ";
+        std::cout << itCria->get_saldo() << " ";
+        std::cout << itCria->get_categoria() << std::endl;
+    }
+
+    for (itAdul = adult.begin(); itAdul != adult.end();itAdul++){// Imprime todos os adultos 
+        std::cout << endl << itAdul->get_id() << " ";
+        std::cout << itAdul->get_nome() << " ";
+        std::cout << itAdul->get_idade() << " ";
+        std::cout << itAdul->get_saldo() << " ";
+        std::cout << itAdul->get_categoria() << std::endl;
+    }
+
+    for (itIdos = idos.begin(); itIdos != idos.end();itIdos++){//Imprime todos os idosos
+        std::cout << endl << itIdos->get_id() << " ";
+        std::cout << itIdos->get_nome() << " ";
+        std::cout << itIdos->get_idade() << " ";
+        std::cout << itIdos->get_saldo() << " ";
+        std::cout << itIdos->get_categoria() << std::endl;
+    }
 }
 
 //procura se o id do usuario existe
