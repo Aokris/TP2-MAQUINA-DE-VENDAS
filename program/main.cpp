ca31a1e4ec317b156d195b59d66e31a059bf69af
Aokris <Aokris@users.noreply.github.com>
2019-11-15 15:54:09 -0300

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -25,16 +25,16 @@ void maquinadevendas(){
     switch (n)
     {
     case 1:
-        cinema.MaquinaDeVendas();
+        //cinema.MaquinaDeVendas();
         break;
     case 2:
-        show.MaquinaDeVendas();
+       // show.MaquinaDeVendas();
         break;
     case 3:
-        boate.MaquinaDeVendas();
+        //boate.MaquinaDeVendas();
         break;
     case 4:
-        fantoche.MaquinaDeVendas();
+        //fantoche.MaquinaDeVendas();
         break;
     default:
         break;
