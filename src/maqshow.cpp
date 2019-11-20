9ac804ac88d9df984d3b8fc7c2ad447740f330ef
RuhannCP <ruhannc@ufmg.br>
2019-11-20 00:49:04 -0200

    Correção maqShow

--- src/maqshow.cpp
+++ src/maqshow.cpp
@@ -4,49 +4,53 @@ using namespace std;
 
 void MaquinaShow::MaquinaDeVendas(){
     
-    cout << " Digite o ID do Filme Desejado " << endl;
+    cout << " Digite o ID do Filme Desejado " << std::endl;
 }
 
 
 void MaquinaShow::VendasShow(vector<Show> show,Adulto adulto){
-    _show = show;
-    int quant = 0, q = 0;
+   
+    int quant = 0, q = 0,n = 0,count = 0;
 
     vector<Show>::iterator it;
 
-    cout<<"VENDA DE INGRESSO: SHOW"<<endl<<endl;
-    cout<<"Favor escolher uma das opções abaixo:"<<endl<<endl;
+    cout<<"VENDA DE INGRESSO: SHOW"<<std::endl<<std::endl;
+    cout<<"Favor escolher uma das opções abaixo:"<<std::endl<<std::endl;
 
-    for(it = _show.begin(); it!=_show.end(); it++){//Imprime as informações 
-        cout<<it->get_id()<<" "<<it->get_nome()<<endl;
+    for(it = show.begin(); it!=show.end(); it++){//Imprime as informações 
+        cout<<it->get_id()<<" "<<it->get_nome() << std::endl;
         std::cout << " - Abertura Portoes: " << it->get_aberturaPortoes();
         
         vector<string> art;
         art = it->get_artistas();
         
-        cout << " - Artistas - " << endl;
-        for (int i = 0; i < sizeof(art);i++){//Imprime os artistas
-            cout << art[i] << endl;    
+        cout << " - Artistas - " << std::endl;
+        for (int i = 0; i < art.size();i++){//Imprime os artistas
+            cout << art[i] << std::endl;    
         }
-        
+        art.clear();
     }
-    std::cout <<endl<<endl;
+    std::cout <<std::endl<<std::endl;
 
-    int n;
-    cout<<"Opcao: ";
-    cin>>n;
-
-    if(n > _show.size()){
+    cout << "Opcao: ";
+    cin >> n;
+    for(it = show.begin(); it != show.end(); it++){
+        if(n == it->get_id()){
+            count++;
+            break;
+        }
+    }
+    if(count == 0){
         std::cout << ("Erro: impossivel encontrar o filme") << std::endl;
         return;
     }
 
     if(n < 0){
-        std::cout << ("Erro: filme nao existente") << std::endl;
+        std::cout << ("Erro: show nao existente") << std::endl;
         return;
     }
 
-    for(it = _show.begin(); it!=_show.end(); it++){//realização de compra
+    for(it = show.begin(); it!=show.end(); it++){//realização de compra
         if (it->get_id() == n){
 
             vector<int> capacidade,preco;
